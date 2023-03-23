aab59ede5f3c171217da03f9916f07f604b6ab5e
kealfeyne <kealfeyneworkshop@gmail.com>
2023-03-23 20:08:04 +0300

    Integrate BertForSequenceClassification

--- ml_model/dataset.py
+++ ml_model/dataset.py
@@ -1,43 +1,16 @@
 import torch
-import numpy as np
-from transformers import BertTokenizer
-
-tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-
-
-# labels = {1: 0,
-#           2: 1,
-#           3: 2,
-#           4: 3,
-#           6: 4,
-#           7: 5,
-#           8: 6,
-#           9: 7,
-#           10: 8}
 
 
 class Dataset(torch.utils.data.Dataset):
-
-    def __init__(self, df):
-        self.labels = df['label'].values
-        self.texts = [tokenizer(text,
-                                padding='max_length', max_length=512, truncation=True,
-                                return_tensors="pt") for text in df['content']]
-
-    def classes(self):
-        return self.labels
-
-    def __len__(self):
-        return len(self.labels)
-
-    def get_batch_labels(self, idx):
-        return np.array(self.labels[idx])
-
-    def get_batch_texts(self, idx):
-        return self.texts[idx]
+    def __init__(self, encodings, labels=None):
+        self.encodings = encodings
+        self.labels = labels
 
     def __getitem__(self, idx):
-        batch_texts = self.get_batch_texts(idx)
-        batch_y = self.get_batch_labels(idx)
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        if self.labels:
+            item["labels"] = torch.tensor(self.labels[idx])
+        return item
 
-        return batch_texts, batch_y
+    def __len__(self):
+        return len(self.encodings["input_ids"])
--- ml_model/train.py
+++ ml_model/train.py
@@ -1,92 +1,67 @@
-import torch
+import numpy as np
 import pandas as pd
-from torch import nn
-from torch.optim import Adam
-from tqdm import tqdm
 import wandb
-from classifier import BertClassifier
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+from transformers import TrainingArguments, Trainer
+from transformers import BertTokenizer, BertForSequenceClassification
 from dataset import Dataset
 
 
-def train(model, train_data, val_data, learning_rate, epochs, batch_size=4):
-    train_ds, val_ds = Dataset(train_data), Dataset(val_data)
+def compute_metrics(p):
+    pred, labels = p
+    pred = np.argmax(pred, axis=1)
 
-    train_dataloader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-    val_dataloader = torch.utils.data.DataLoader(val_ds, batch_size=batch_size)
+    accuracy = accuracy_score(y_true=labels, y_pred=pred)
+    recall = recall_score(y_true=labels, y_pred=pred)
+    precision = precision_score(y_true=labels, y_pred=pred)
+    f1 = f1_score(y_true=labels, y_pred=pred)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=learning_rate)
-
-    if torch.cuda.is_available():
-        model = model.cuda()
-        criterion = criterion.cuda()
-
-    run = wandb.init(project="Greenatom", name='bert_finetuned')
-
-    for epoch_num in range(epochs):
-
-        total_acc_train = 0
-        total_loss_train = 0
-
-        for train_input, train_label in tqdm(train_dataloader):
-            train_label = train_label.to(device)
-            mask = train_input['attention_mask'].to(device)
-            input_id = train_input['input_ids'].squeeze(1).to(device)
-
-            output = model(input_id, mask)
-
-            batch_loss = criterion(output, train_label.long())
-            total_loss_train += batch_loss.item()
-
-            acc = (output.argmax(dim=1) == train_label).sum().item()
-            total_acc_train += acc
-
-            model.zero_grad()
-            batch_loss.backward()
-            optimizer.step()
-
-        total_acc_val = 0
-        total_loss_val = 0
-
-        with torch.no_grad():
-
-            for val_input, val_label in val_dataloader:
-                val_label = val_label.to(device)
-                mask = val_input['attention_mask'].to(device)
-                input_id = val_input['input_ids'].squeeze(1).to(device)
-
-                output = model(input_id, mask)
-
-                batch_loss = criterion(output, val_label.long())
-                total_loss_val += batch_loss.item()
-
-                acc = (output.argmax(dim=1) == val_label).sum().item()
-                total_acc_val += acc
-
-        torch.save(model.state_dict(), f"../trained_models/bert_base_uncased_{epoch_num}epochs.pt")
-
-        print(
-            f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f}\
-                | Train Accuracy: {total_acc_train / len(train_data): .3f}\
-                | Val Loss: {total_loss_val / len(val_data): .3f}\
-                | Val Accuracy: {total_acc_val / len(val_data): .3f}')
-
-        wandb.log({
-            'epoch': epoch_num + 1,
-            'train_loss': total_loss_train / len(train_data),
-            'train_acc': total_acc_train / len(train_data),
-            'val_loss': total_loss_val / len(val_data),
-            'val_acc': total_acc_val / len(val_data)
-        })
-
-    wandb.finish()
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
 
 
-train(model=BertClassifier(),
-      train_data=pd.read_csv("../data/train.csv"),
-      val_data=pd.read_csv("../data/test.csv"),
-      learning_rate=1e-5,
-      epochs=10,
-      batch_size=2)
+train_data = pd.read_csv("../data/train.csv").iloc[0:2000]
+val_data = pd.read_csv("../data/test.csv").iloc[0:2000]
+
+model_name = "distilbert-base-uncased"
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForSequenceClassification.from_pretrained(model_name, num_labels=9)
+
+label_mapping = {1: 0, 2: 1, 3: 2, 4: 3, 6: 4, 7: 5, 8: 6, 9: 7, 10: 8}
+
+X_train, y_train = list(train_data["content"]), [label_mapping[x] for x in train_data["label"]]
+X_train_tokenized = tokenizer(X_train, padding=True, truncation=True, max_length=512)
+
+X_val, y_val = list(val_data["content"]), [label_mapping[x] for x in val_data["label"]]
+X_val_tokenized = tokenizer(X_val, padding=True, truncation=True, max_length=512)
+
+train_dataset = Dataset(X_train_tokenized, y_train)
+val_dataset = Dataset(X_val_tokenized, y_val)
+
+wandb.init(project="Greenatom", name="bertforseqclass")
+
+args = TrainingArguments(
+    output_dir="bertforaeqclass_trained_models",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    # eval_steps=500,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
+    num_train_epochs=3,
+    seed=0,
+    load_best_model_at_end=True,
+    report_to=["wandb"]
+)
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+
+trainer.save_model('BertForSeqClass_trained_models')
+trainer.save_state()
+
+wandb.finish()
